70603caeee9ac71cebda06a4dcfd4a6e5bc4ff58
Chinmay <chinz.sandur07@gmail.com>
2023-11-24 13:03:11 +0530

    add logic to checkWin

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let music = new Audio("music.mp3");
 let audioTurn = new Audio("ting.mp3");
 let gameover = new Audio("gameover.mp3");
 let turn = "X";
+let gameOver = false;
 
 //Function to change the turn
 const changeTurn = () => {
@@ -13,7 +14,24 @@ const changeTurn = () => {
 //function to check for a win
 
 const checkwin = () => {
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e => {
+        if ((boxtext[e[0]].innerText === boxtext[e[1].innerText]) && (boxtext[e[2]].innerText === boxtext[e[1].innerText]) && (boxtext[e[0]].innerText !== "")) {
+            document.querySelector(".info").innerText = boxtext[e[0]].innerText + "Won";
+            gameOver = true;
+        }
 
+    })
 }
 
 // Game logic
@@ -26,7 +44,9 @@ Array.from(boxes).forEach(element => {
             turn = changeTurn();
             audioTurn.play();
             checkwin();
-            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            if (!gameOver) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
         }
     })
 })
