7d3f4fd71107fa130207d46a72bee3f1fa68ba46
Chinmay <chinz.sandur07@gmail.com>
2023-11-24 11:08:26 +0530

    add function for changeOfTurn

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Welcome")
+
+let music = new Audio("music.mp3");
+let turn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let Turn = "X";
+
+//Function to change the turn
+const changeTurn = () => {
+    return Turn === "X" ? 0 :"X";
+}
